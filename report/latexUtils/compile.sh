c3a4336d5d2934c8f137d9d1f75cc638f2a0046c
Anders <anders.nissum@live.dk>
2025-12-15 14:00:39 +0100

    test 2 compile fix

--- report/latexUtils/compile.sh
+++ report/latexUtils/compile.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+#!/bin/bash
+
 if [ "$1" == "reflection" ]; then
-    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
+    cd /reflection
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    biber main  # Add this step if using biber
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    rm -f *.aux *.log *.out *.synctex.gz
 elif [ "$1" == "groupreport" ]; then
-    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
+    cd /groupreport
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    biber main  # Add this step if using biber
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    rm -f *.aux *.log *.out *.synctex.gz
 else
-    cd /reflection && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
-    cd /groupreport && xelatex -synctex=1 -interaction=nonstopmode main.tex && rm -f *.aux *.log *.out *.synctex.gz
+    cd /reflection
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    biber main  # Add this step if using biber
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    rm -f *.aux *.log *.out *.synctex.gz
+
+    cd /groupreport
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    biber main  # Add this step if using biber
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    xelatex -synctex=1 -interaction=nonstopmode main.tex
+    rm -f *.aux *.log *.out *.synctex.gz
 fi
+
